8897893a09d536fb39d505b8bd6096d1cf3c1f6f
MaestroFoxOz33 <jo.troadec@gmail.com>
2017-05-19 16:56:11 +0200

    Ajout fichier config

---
+++ src/main/java/com/cesi/Config.java
@@ -0,0 +1,19 @@
+package com.cesi;
+
+public class Config {
+
+    public static String getUrl(){
+        return "jdbc:mysql://localhost:3306/mediatheque";
+    }
+
+    public static String getUser(){
+        return "root";
+    }
+
+    public static String getPwd(){
+        return "Mandji135";
+    }
+
+
+}
+
