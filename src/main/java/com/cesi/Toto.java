3a1f3a1e4fb40618969fbb0ae8eaa6e5f5e767a6
acydcytryk <acydcytryk@gmail.com>
2017-05-23 14:29:55 +0200

    feat : ajout fx bouton + connexion

---
+++ src/main/java/com/cesi/Toto.java
@@ -0,0 +1,54 @@
+package com.cesi;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Toto implements Initializable{
+
+    @FXML
+    Button bOk;
+    @FXML
+    Label lbTest;
+
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("-------- MySQL JDBC Connection Testing ------------");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your MySQL JDBC Driver?");
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("MySQL JDBC Driver Registered!");
+        Connection connection = null;
+        try {
+            connection = DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/tchat","root", "root");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console");
+            e.printStackTrace();
+            return;
+        }
+        if (connection != null) {
+            System.out.println("You made it, take control your database now!");
+        } else {
+            System.out.println("Failed to make connection!");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        bOk.setOnAction(this::handleButtonAction);
+    }
+
+
+}
