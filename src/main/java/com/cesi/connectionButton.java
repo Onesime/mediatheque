ecd0e00b3b5ff6962609b92c8bea3ff80d940084
acydcytryk <acydcytryk@gmail.com>
2017-05-24 09:36:42 +0200

    feat : rename + clean code

---
+++ src/main/java/com/cesi/connectionButton.java
@@ -0,0 +1,29 @@
+package com.cesi;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class connectionButton implements Initializable{
+
+    @FXML
+    Button bOk;
+    @FXML
+    Label lbTest;
+
+    private void handleButtonAction(ActionEvent event) {
+        Connect.connect();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+        bOk.setOnAction(this::handleButtonAction);
+    }
+
+
+}
