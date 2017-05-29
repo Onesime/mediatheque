bdc4434c86a631af7ae4d543ef5fae0013ff0368
acydcytryk <acydcytryk@gmail.com>
2017-05-29 12:35:43 +0200

    feat : MAJ

--- src/main/java/model/DAOOeuvre.java
+++ src/main/java/model/DAOOeuvre.java
@@ -15,8 +15,15 @@ import javafx.beans.property.StringProperty;
 
 public class DAOOeuvre {
 
-	static String selectSQL = "SELECT oeuvre.*, auteur.name, genre.name, langue.name FROM oeuvre INNER JOIN auteur ON oeuvre.auteur_id=auteur.id " +
-			"INNER JOIN genre ON oeuvre.genre_id=genre.id " + "INNER JOIN langue ON oeuvre.langue_id=langue.id " +
+	static String selectSQL = "SELECT oeuvre.*, auteur.name, genre.name, langue.name, categorie.name, " +
+			"origine.name, plateforme.name, morceau.name FROM oeuvre " +
+			"LEFT JOIN morceau ON oeuvre.id=morceau.oeuvre_id " +
+			"LEFT JOIN plateforme ON oeuvre.plateforme_id=plateforme.id " +
+			"INNER JOIN origine ON oeuvre.origine_id=origine.id " +
+			"INNER JOIN categorie ON oeuvre.categorie_id=categorie.id " +
+			"INNER JOIN auteur ON oeuvre.auteur_id=auteur.id " +
+			"INNER JOIN genre ON oeuvre.genre_id=genre.id " +
+			"INNER JOIN langue ON oeuvre.langue_id=langue.id " +
 			"WHERE oeuvre.id=?";
 
 	public static void main(String[] argv) {
@@ -35,7 +42,7 @@ public class DAOOeuvre {
 		try	{
 			PreparedStatement preparedStatement = connection.prepareStatement(selectSQL);
 
-			preparedStatement.setInt(1, 8);
+			preparedStatement.setInt(1, 11);
 
 			/// result
 			ResultSet rs = preparedStatement.executeQuery();
@@ -58,6 +65,10 @@ public class DAOOeuvre {
 				System.out.println(rs.getString("auteur.name"));
 				System.out.println(rs.getString("genre.name"));
 				System.out.println(rs.getString("langue.name"));
+				System.out.println(rs.getString("categorie.name"));
+				System.out.println(rs.getString("origine.name"));
+				System.out.println(rs.getString("plateforme.name"));
+				System.out.println(rs.getString("morceau.name"));
 				/*
 				oeuvre.setId(rs.getInt("id"));
 				oeuvre.setTitre(rs.getString("titre"));
