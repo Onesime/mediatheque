507e60cd0cb81bcf625227e41950e683cc8a7a3f
MaestroFoxOz33 <jo.troadec@gmail.com>
2017-06-01 15:47:25 +0200

    ajout fxml : ad, mod, suppr. Modif ConnectionButton pour test

--- src/main/java/com/cesi/ConnectionButton.java
+++ src/main/java/com/cesi/ConnectionButton.java
@@ -4,25 +4,28 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import scene.AdOeuvreScene;
 
 public class ConnectionButton implements Initializable{
+    protected final FXMLLoader loader = new FXMLLoader();
+    protected final Stage stage = new Stage();
 
     @FXML
-    Button bOk;
-    @FXML
-    Label lbTest;
+    Button ad_oeuvre;
 
     private void handleButtonAction(ActionEvent event) {
-        //Connect.connect();
+        ad_oeuvre.setOnAction(loader.setLocation(Main.class.getResource("/ad.oeuvre.fxml")).show());
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // TODO Auto-generated method stub
-        bOk.setOnAction(this::handleButtonAction);
+        ad_oeuvre.setOnAction(this::handleButtonAction);
     }
 
 }
---
+++ src/main/java/scene/AccueilScene.java
@@ -0,0 +1,7 @@
+package scene;
+
+/**
+ * Created by Jordan TROADEC on 01/06/2017.
+ */
+public class AccueilScene {
+}
